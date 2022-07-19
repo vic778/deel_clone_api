c92c79816df799ded42fdeb87992aecb61bfbb98
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-19 14:40:36 +0300

    Add prifile model to admin pannel

---
+++ app/admin/profiles.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Profile do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :first_name, :last_name, :date_of_birth, :country, :avatar_url, :video_url, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:first_name, :last_name, :date_of_birth, :country, :avatar_url, :video_url, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
