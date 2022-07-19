a10560f5b4a78016265179df0acb30708ab4b429
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-19 17:30:03 +0300

    Migrate contract

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_18_163640) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_140219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_18_163640) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "contracts", force: :cascade do |t|
+    t.integer "name", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_contracts_on_name"
+    t.index ["user_id"], name: "index_contracts_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -95,6 +104,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_18_163640) do
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "contracts", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "users", "roles"
 end
