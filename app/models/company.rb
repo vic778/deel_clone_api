564f0cefcd6dbc1c12a5a93810b8c7ce6b49120c
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-20 12:10:42 +0300

    Validate and make unique some values

---
+++ app/models/company.rb
@@ -0,0 +1,8 @@
+class Company < ApplicationRecord
+  belongs_to :profile
+
+  validates :legal_name, presence: true, uniqueness: true
+  validates :company_type, presence: true
+  validates :registration_number, presence: true, uniqueness: true
+
+end
