d78437b0f710427834016c079d47d708b3a83834
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-20 11:25:45 +0300

    Validate presence of date

--- app/models/contract_detail.rb
+++ app/models/contract_detail.rb
@@ -1,10 +1,16 @@
 class ContractDetail < ApplicationRecord
   belongs_to :contract
-  
+  before_save :validate_start_date
+
   validates :contract_name, presence: true
   validates :contractor_residence, presence: true
   validates :job_title, presence: true
   validates :description, presence: true
-  validates :start_date, presence: true
+  validates :start_date, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "must be in the format YYYY-MM-DD" }
 
+  # check if start_date is not behind today's data
+  def validate_start_date
+    @date = DateTime.now
+    errors.add(:start_date, "can't be in the past") if start_date.present? && start_date < @date
+  end
 end
