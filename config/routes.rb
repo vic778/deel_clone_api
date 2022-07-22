19beb9de872816945da495d60626d4e7636846af
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-22 15:32:57 +0300

    Add payemtn route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
       resources :contract_details, only: [:create, :update]
     end
 
-    resources :companies 
+    resources :companies
+    resources :payment
 
    
   end
