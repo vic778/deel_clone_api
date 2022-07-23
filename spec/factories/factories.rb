3001575efa5df6032353645a02f87b17a45efc84
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-23 17:09:59 +0300

    Fix linters error

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -6,4 +6,69 @@ FactoryBot.define do
       instance.reload
     end
   end
+
+  factory :user do
+    username { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { 'password' }
+    password_confirmation { 'password' }
+
+    association :role, factory: :role, name: 'user'
+
+    trait :admin do
+      association :role, factory: :role, name: 'cpmpany'
+    end
+
+    trait :teacher do
+      association :role, factory: :role, name: 'admin'
+    end
+  end
+
+  factory :profile do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone { Faker::PhoneNumber.phone_number }
+    address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    zip { Faker::Address.zip_code }
+    country { Faker::Address.country }
+
+    association :user, factory: :user
+  end
+
+  factory :company do
+    legal_name { Faker::Company.name }
+    company_type { Faker::Company.type }
+    phone_number { Faker::PhoneNumber.phone_number }
+    registration_number { Faker::Company.swedish_organisation_number }
+    vat_id { Faker::Company.swedish_organisation_number }
+
+    association :profile, factory: :profile
+  end
+
+  factory :contract do
+    name { Faker::Name.name }
+    association :company, factory: :company
+    association :user, factory: :usert
+  end
+
+  factory :payment do
+    amount { Faker::Number.number(digits: 2) }
+    status { Payment.statuses.keys.sample }
+
+    association :contract, factory: :contract
+    association :user, factory: :user
+  end
+
+  factory :contract_detail do
+    contract_name { Faker::Name.name }
+    contractor_residence { Faker::Address.street_address }
+    job_title { Faker::Name.title }
+    description { Faker::Lorem.paragraph }
+    start_date { Faker::Date.forward(days: 30) }
+    over_date { Faker::Date.forward(days: 30) }
+
+    association :contract, factory: :contract
+  end
 end
