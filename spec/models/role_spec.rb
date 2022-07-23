eb5e5e79c63ed8ae4f749d374050202daa3acb07
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-23 17:31:52 +0300

    Describe the block to not be valide without the name

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:users) }
+  it {is_expected.to validate_presence_of(:name) }
+
+  # describe 'validations' do
+  #   it { should validate_uniqueness_of(:name).case_insensitive }
+  # end
+
+  describe 'It is invalid without the name' do
+    it 'should be invalid' do
+      role = build(:role, name: nil)
+      expect(role).to_not be_valid
+    end
+  end
 end
