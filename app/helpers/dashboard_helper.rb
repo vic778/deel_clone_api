9d8f931f1e9858765f8b09876382d6f873c4189a
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-23 10:24:25 +0300

    Create history action

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,38 @@
 module DashboardHelper
+
+    #display the balance in the contract where the user is currently logged in
+    def balance_display
+        contract = Contract.where(user_id: current_user.id).first
+        payment = Payment.where(contract_id: contract.id).first
+    end
+
+    def contract_user
+        contract = Contract.where(user_id: current_user.id).first
+    end
+
+    def contract_details
+        contract = Contract.where(user_id: current_user.id).first
+        contract_details = ContractDetail.where(contract_id: contract.id).first
+    end
+
+    def hostori
+        contract = Contract.where(user_id: current_user.id).all
+        payment = Payment.where(contract_id: contract.id).all
+    end
+
+    def upcoming 
+        contract = Contract.where(user_id: current_user.id)
+        payment = Payment.where(user_id: current_user.id, status: 'pending')
+    end
+
+    def payment_pub
+        contract = Contract.where(user_id: current_user.id)
+        payment = Payment.where(user_id: current_user.id, status: 'published')
+    end
+
+    def payment_hist
+        contract = Contract.where(user_id: current_user.id)
+        payment = Payment.where(user_id: current_user.id).all
+    end
+
 end
