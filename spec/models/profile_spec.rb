7bf21eb7fae14ece9b82c505f81b71a8e5dc8f56
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-23 17:39:49 +0300

    Associate to user

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to(:user) }
+  it {should have_many(:companies) }
 end
