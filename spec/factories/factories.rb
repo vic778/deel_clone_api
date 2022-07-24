28eb9bab93482c3b71110b1f6f5708ab72f80851
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-24 16:34:47 +0300

    TEst user

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
   end
 
   factory :user do
-    username { Faker::Name.name }
+    username { Faker::Internet.user_name }
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
