1dcba4a936dc3cdb90a8155d8d2f045c9a015b7b
vic778 <victoiremmanuelbarh@gmail.com>
2022-09-07 19:17:36 +0300

    Get all companies for current_user

--- spec/requests/api/companies_spec.rb
+++ spec/requests/api/companies_spec.rb
@@ -29,5 +29,69 @@ RSpec.describe 'api/companies', type: :request do
         end
       end
     end
+
+    path "/api/companies/{id}" do
+      put "Update a company" do
+        tags "Companies"
+        consumes "application/json"
+        security [bearer_auth: {}]
+        parameter name: :id, in: :path, type: :string
+        parameter name: :company, in: :body, schema: {
+          type: :object,
+          properties: {
+            legal_name: { type: :string },
+            company_type: { type: :string },
+            registration_number: { type: :integer },
+            phone_number: { type: :string },
+            vat_id: { type: :string }
+          },
+          required: %w[legal_name company_type registration_number phone_number vat_id]
+        }
+        response "200", "company updated" do
+          let(:company) { { legal_name: "Company 1", company_type: "Sole Proprietorship", registration_number: 123_456_789, phone_number: "123456789", vat_id: "123456789" } }
+          run_test!
+        end
+
+        response "422", "invalid request" do
+          let(:company) { { name: "Company 1", address: "123 Main St", city: "City", state: "State", zip: "12345", phone: "123-456-7890" } }
+          run_test!
+        end
+      end
+    end
+
+    path "/api/companies/{id}" do
+      delete "Delete a company" do
+        tags "Companies"
+        consumes "application/json"
+        security [bearer_auth: {}]
+        parameter name: :id, in: :path, type: :string
+        response "204", "company deleted" do
+          run_test!
+        end
+      end
+    end
+
+    path "/api/companies/{id}" do
+      get "Show a company" do
+        tags "Companies"
+        consumes "application/json"
+        security [bearer_auth: {}]
+        parameter name: :id, in: :path, type: :string
+        response "200", "company found" do
+          run_test!
+        end
+      end
+    end
+
+    path "/api/companies" do
+      get "List all companies for current user" do
+        tags "Companies"
+        consumes "application/json"
+        security [bearer_auth: {}]
+        response "200", "companies found" do
+          run_test!
+        end
+      end
+    end
   end
 end
