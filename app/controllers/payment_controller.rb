6b19dd81619d801e8b44cfd93f0b363d8037354a
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-12 22:16:00 +0300

    Find employee before any action

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -3,7 +3,6 @@ class PaymentController < PermissionsController
   before_action :set_payment, only: %i[show update destroy]
 
   def create
-    @ad = current_user
     @user = User.find(params[:user_id])
     #  byebug
     @contract = Contract.where(id: params[:contract_id], user_id: params[:user_id])
@@ -11,9 +10,9 @@ class PaymentController < PermissionsController
     # check is the user in the params is in the contract
     if @contract.exists?
       # create payment
-      @payment = Payment.new(payment_params.merge(user: @ad.id))
+      @payment = Payment.new(payment_params.merge(user: @user))
       @payment.contract = @contract
-      @payment.user = @user
+      # @payment.user = @user
       @payment.save
       render json: @payment, status: :created
     # @contract.update(payment_status: true)
