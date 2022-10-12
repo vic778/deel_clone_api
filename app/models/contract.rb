edd076fce8ce0c54f913ee64ed326e075d19e57e
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-12 23:30:11 +0300

    Associate to employee

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,7 +2,7 @@ class Contract < ApplicationRecord
   belongs_to :user
   belongs_to :employee, class_name: 'User', foreign_key: 'employee_id'
   belongs_to :company
-  has_many :payment
+  has_many :payments
   has_one :contract_detail
 
   enum name: {
