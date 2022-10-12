c5505cbd0cfdb553e147c379a43557a4b1ff5ba8
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-12 23:29:38 +0300

    Check if a user has a contract

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,24 +1,20 @@
 class PaymentController < PermissionsController
   before_action :authenticate_user!
   before_action :set_payment, only: %i[show update destroy]
+  before_action :check_use_in_contract, only: %i[create]
 
   def create
     @user = User.find(params[:user_id])
     #  byebug
-    @contract = Contract.where(id: params[:contract_id], user_id: params[:user_id])
-    # puts "comment params: #{@contract}"
-    # check is the user in the params is in the contract
-    if @contract.exists?
-      # create payment
-      @payment = Payment.new(payment_params.merge(user: @user))
-      @payment.contract = @contract
-      # @payment.user = @user
-      @payment.save
+    @contract = Contract.find(params[:contract_id])
+    @payment = Payment.new(payment_params)
+    @payment.contract = @contract
+    @payment.user = @user
+    if @payment.save
       render json: @payment, status: :created
     # @contract.update(payment_status: true)
-
     else
-      render json: { error: "User not in contract" }, status: :unprocessable_entity
+      render json: { errors: @payment.errors.full_messages }, status: :unprocessable_entity
     end
 
     # Rails.logger.debug "PaymentController#create: #{@contract.inspect}"
@@ -32,5 +28,11 @@ class PaymentController < PermissionsController
 
   def payment_params
     params.permit(:amount, :contract_id, :user_id)
+  end
+
+  def check_use_in_contract
+    @user = User.find(params[:user_id])
+    @contract = Contract.find(params[:contract_id])
+    render json: { errors: 'You do not have a contract with this user' }, status: :unprocessable_entity if @contract.user != @user
   end
 end
