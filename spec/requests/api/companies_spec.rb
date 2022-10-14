12fdf0dc1798ad6e431567b040ff6ee44cef0abb
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-14 15:14:33 +0300

    Decode token for Authorization

--- spec/requests/api/companies_spec.rb
+++ spec/requests/api/companies_spec.rb
@@ -19,10 +19,7 @@ RSpec.describe 'api/companies', type: :request do
           required: %w[legal_name company_type registration_number phone_number vat_id]
         }
         response "201", "company created" do
-          let(:user) { create(:user, :admin) }
-          let(:Authorization) { "Bearer #{user.jwt}" }
           let(:company) { { legal_name: "Company 1", company_type: "Sole Proprietorship", registration_number: 123_456_789, phone_number: "123456789", vat_id: "123456789" } }
-          # run_test!
         end
 
         response "422", "invalid request" do
--- spec/requests/api/dashboard_spec.rb
+++ spec/requests/api/dashboard_spec.rb
@@ -1,36 +1,42 @@
-# require 'swagger_helper'
+require 'swagger_helper'
 
-# RSpec.describe 'api/dashboard', type: :request do
-#   path "/api/dashboard" do
-#     get "Get dashboard data" do
-#       tags "Dashboard"
-#       consumes "application/json"
-#       security [bearer_auth: {}]
-#       response "200", "dashboard data" do
-#         run_test!
-#       end
-#     end
-#   end
+RSpec.describe 'api/dashboard', type: :request do
+  path "/api/dashboard" do
+    get "Get dashboard data" do
+      tags "Dashboard"
+      consumes "application/json"
+      security [bearer_auth: {}]
+    
+      response '201', 'successfully authenticated' do
+        token = JWT.encode({ id: 1 }, 'vicSecret', 'HS256')
+        let(:Authorization) { "Bearer #{token}" }
+      end
+    end
+  end
 
-#   path "/api/dashboard/contracts" do
-#     get "Get the details of your contract" do
-#       tags "Dashboard"
-#       consumes "application/json"
-#       security [bearer_auth: {}]
-#       response "200", "dashboard contracts data" do
-#         run_test!
-#       end
-#     end
-#   end
+  path "/api/dashboard/contracts" do
+    get "Get the details of your contract" do
+      tags "Dashboard"
+      consumes "application/json"
+      security [bearer_auth: {}]
+      response "200", "dashboard contracts data" do
+        let(:user) { create(:user, :admin) }
+        let(:Authorization) { "Bearer #{@token}" }
+       
+      end
+    end
+  end
 
-#   path "/api/dashboard/upcoming" do
-#     get "See your upcoming payment" do
-#       tags "Dashboard"
-#       consumes "application/json"
-#       security [bearer_auth: {}]
-#       response "200", "dashboard upcoming data" do
-#         run_test!
-#       end
-#     end
-#   end
-# end
+  path "/api/dashboard/upcoming" do
+    get "See your upcoming payment" do
+      tags "Dashboard"
+      consumes "application/json"
+      security [bearer_auth: {}]
+      response "200", "dashboard upcoming data" do
+        let(:user) { create(:user, :admin) }
+        let(:Authorization) { "Bearer #{@token}" }
+        # run_test!
+      end
+    end
+  end
+end
